15e728d0eb7d048279f6c975cdcceb85501f365c
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-12-04 15:58:10 +0300

    Example1 added

---
+++ Example1/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите программу, которая принимает на вход два числа (A и B) и 
+метод который возводит число A в натуральную степень B. 
+Задание должно быть выполнено в методе. 
+В методе не должно быть вывода в консоль. Не использовать Math.Pow()
+ */
+
+bool isParsedA = int.TryParse(Console.ReadLine(), out int A);
+bool isParsedB = int.TryParse(Console.ReadLine(), out int B);
+if (!isParsedA || !isParsedB)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+}
+
+int Exponent = GettingExponent(A, B);
+
+Console.WriteLine(Exponent);
+
+
+
+int GettingExponent(int A, int B)
+{
+    int count = 1;
+    int result = A;
+    while (count < B)
+    {
+        result = result * A;
+        count++;
+    } 
+    return result;
+}
